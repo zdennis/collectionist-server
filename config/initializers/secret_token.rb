b139cfe9c69827ef36533c381d62f70d87329496
Matthew Seeley <matthew@threadlight.com>
2012-04-16 11:08:16 -0400

    Initial Rails 3 / MySQL project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollectionistServer::Application.config.secret_token = 'f102ac4a688aa17715455171b8cdf12303c6db91c7e7c9c99fa21bb4f51c49c233eac71dab6505d6de67d95363246d7b164e4deaa635c7fd4916051e54b07189'
